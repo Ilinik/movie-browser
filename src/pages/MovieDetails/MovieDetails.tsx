0827b4975e850d0ce6067fb04e3e14c54ffc11c2
acoji <acoji.gm@gmail.com>
2025-12-10 10:34:00 +0300

    feat: add ui layout for movie details

--- src/pages/MovieDetails/MovieDetails.tsx
+++ src/pages/MovieDetails/MovieDetails.tsx
@@ -4,9 +4,53 @@ import { useMovieDetails } from '../../hooks/movies/useMovieDetails';
 export const MovieDetails = () => {
   const { id } = useParams();
 
-  const { data: movieDetails } = useMovieDetails(Number(id));
+  const {
+    data: movieDetails,
+    isLoading,
+    isError,
+  } = useMovieDetails(Number(id));
 
-  console.log(movieDetails);
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error</div>;
 
-  return <div>{movieDetails?.title}</div>;
+  return (
+    <section className="section">
+      <div className="flex gap-8">
+        <img
+          className="object-cover rounded-lg max-w-[220px] w-full h-auto"
+          src={`${import.meta.env.VITE_API_IMAGE}/w300${movieDetails.poster_path}`}
+          alt={movieDetails.title}
+        />
+        <div className="pt-6 pb-6 flex flex-col gap-5">
+          <div className="flex flex-col gap-3 max-w-220 w-full">
+            <h2 className="font-bold text-3xl">{movieDetails.title}</h2>
+            <p className="text-md text-gray-600 dark:text-gray-300">
+              {movieDetails.overview}
+            </p>
+          </div>
+          <div
+            className="flex flex-col gap-3 relative 
+            before:content-[''] before:absolute before:left-0 before:top-0 
+            before:w-full before:h-px before:bg-gray-600 before:opacity-30 py-5"
+          >
+            <p
+              className="px-4 py-2 bg-green-700 rounded-full font-medium self-start 
+              transition-all duration-300 hover:scale-105 hover:shadow-lg text-white"
+            >
+              {movieDetails.status}
+            </p>
+
+            <div className="flex flex-col gap-2">
+              <p className="font-bold">Tagline</p>
+              <p>
+                {movieDetails.tagline.length === 0
+                  ? 'Tagline not found'
+                  : movieDetails.tagline}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
